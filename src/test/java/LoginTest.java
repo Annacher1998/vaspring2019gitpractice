10f20936057585ee7d2b313cfe5d7d41b08e1d82
Anna Chernyshova <50154954+Annacher1998@users.noreply.github.com>
2019-08-06 21:39:42 -0400

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,11 +1,8 @@
 public class LoginTest {
 
     public static void main(String[] args) {
-        System.out.println("line 1");
-        System.out.println("line 2");
-        System.out.println("line 3");
-        System.out.println("line 4");
-        System.out.println("line 5");
+        System.out.println("line 177");
+       
 
     }
 }
