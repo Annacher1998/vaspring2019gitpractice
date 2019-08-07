53d71f8ad8bd54b78bb958726a811b2bcac3a2e1
Annacher1998 <chernyshova.anna1998@gmail.com>
2019-08-06 21:39:05 -0400

    changing file again

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,12 +2,7 @@ public class LoginTest {
 
     public static void main(String[] args) {
         System.out.println("line 1");
-        System.out.println("line 2");
-        System.out.println("line 3");
-        System.out.println("line 4");
-        System.out.println("line 5");
-        System.out.println("line 6");
-        System.out.println("line 7");
+
 
     }
 }
