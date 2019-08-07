a2c0daa5161feba5d944e2827fcb4fafe12489d5
Annacher1998 <chernyshova.anna1998@gmail.com>
2019-08-06 21:20:35 -0400

    merged both files

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,6 +2,6 @@ public class AppTest {
 
     public static void main(String[] args){
         System.out.println("Good morning");
-       
+
     }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,7 +4,7 @@ public class LoginTest {
         System.out.println("line 1");
         System.out.println("line 2");
         System.out.println("line 3");
-
+        System.out.println("line 4");
 
     }
 }
